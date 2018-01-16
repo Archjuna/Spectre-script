821437191a79707d5224411eb80790dd556e567d
Arch <arch@Arch-portable.localdomain>
2018-01-16 19:59:45 +0100

    Gestion erreur

--- script_spectre.sh
+++ script_spectre.sh
@@ -4,11 +4,12 @@
 
 if [ -f /bin/wget ]; then
 	wget --progress=bar -O script_check.sh https://raw.githubusercontent.com/speed47/spectre-meltdown-checker/master/spectre-meltdown-checker.sh
+	chmod +x script_check.sh
+	sudo ./script_check.sh | grep '\(CVE\|STATUS\)'
+	echo "Pensez à mettre à jour votre BIOS (si possible), vos navigateur web et vos distribution (+ microcode)"
+	rm script_check.sh
 else
 	echo "Merci d'installer wget !"
 fi
-chmod +x script_check.sh
-sudo ./script_check.sh | grep '\(CVE\|STATUS\)'
-echo "Pensez à mettre à jour votre BIOS (si possible), vos navigateur web et vos distribution (+ microcode)"
-rm script_check.sh
+
 
