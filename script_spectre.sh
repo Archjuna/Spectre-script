57019b4854bc4566b5d95b8c52cf2fdd8aaa6fcd
Arch <arch@Arch-portable.localdomain>
2018-01-16 11:53:14 +0100

    script de test

---
+++ script_spectre.sh
@@ -0,0 +1,14 @@
+# !/bin/sh
+# Script pour vérifier la vulnérabilité du systeme sur les failles spectre et meltdown
+# Pour plus d'info aller voir : https://github.com/speed47/spectre-meltdown-checker
+
+if [ -f /bin/wget ]; then
+	wget --progress=bar -O script_check.sh https://raw.githubusercontent.com/speed47/spectre-meltdown-checker/master/spectre-meltdown-checker.sh
+else
+	echo "Merci d'installer wget !"
+fi
+chmod +x script_check.sh
+sudo ./script_check.sh | grep '\(CVE\|STATUS\)'
+echo "Pensez à mettre à jour votre BIOS (si possible), vos navigateur web et vos distribution (+ microcode)"
+rm script_check.sh
+
